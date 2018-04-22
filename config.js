212e0d0d61a0284ee7047e63d447705adaf891d0
Apoorva Saxena <apsaxena24@gmail.com>
2018-04-22 23:31:13 +0100

    More files

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  usernamestt: process.env.USERNAMESTT,
+  passstt: process.env.PASSSTT,
+  usernametts: process.env.USERNAMETTS,
+  passtts: process.env.PASSTTS
+}
+
+module.exports = config
