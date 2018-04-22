59def634302c738828fc3d1e08188bce23a59bb3
Apoorva Saxena <apsaxena24@gmail.com>
2018-04-23 00:04:25 +0100

    Identify and translation begins

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const config = {
   usernamestt: process.env.USERNAMESTT,
   passstt: process.env.PASSSTT,
   usernametts: process.env.USERNAMETTS,
-  passtts: process.env.PASSTTS
+  passtts: process.env.PASSTTS,
+  usernametrans: process.env.USERNAMETRANS,
+  passtrans: process.env.PASSTRANS
 }
 
 module.exports = config
